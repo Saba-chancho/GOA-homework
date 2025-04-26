2aaa11e91f49311f8db2588a928667a96085c2c5
Saba-chancho <geosaba215@gmail.com>
2025-04-26 15:20:29 +0400

    third commit

--- day 01/homework/lesson.py
+++ day 01/homework/lesson.py
@@ -4,3 +4,20 @@ age = "14"
 height = "171"
 
 print("my name is " + name + " " +  surname + ", my age is " + age + " and my height is " + height + "cm")
+
+name = "saba"
+color1 = 'black'
+color2 = "white"
+age = 15
+friend1 = 15
+friend2 = 14
+mom = "feride"
+dad = "lasha"
+sister1 = "elene"
+sister2 = "sesili"
+pet = dog True
+
+#String = str ნებისმიერი ტექსტი რომელიც არის "" მოქცეული "hello world"
+#Integer = int მთელი რიცხვი 15
+#float = float წილადი რიცხვი 26.3
+#boolean = bool მართლია,მცდარია true/false
---
+++ day 02/homework/lesson.py
@@ -0,0 +1,18 @@
+my_name = "saba"
+favorite_color1 = 'black'
+favorite_color2 = "white"
+my_age = 15
+friend1_age = 15
+friend2_age = 14
+mom_name = "feride"
+dad_name = "lasha"
+sister1_name = "elene"
+sister2_name = "sesili"
+have_pet = True
+have_bird = False
+
+
+#String = str -> ნებისმიერი ტექსტი, რომელიც არის " " მოქცეული, მაგალითად: "hello world"
+#Integer = int -> მთელი რიცხვი, მაგალითად: 15
+#float = float -> წილადი რიცხვი, მაგალითად: 26.3
+#boolean = bool -> მართალია ან მცდარია, True/False
