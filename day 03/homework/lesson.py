d254a2d0fa01cd99149ca5b7eb2a74ecb71a6e1a
Saba-chancho <geosaba215@gmail.com>
2025-04-29 19:02:02 +0400

    commit

---
+++ day 03/homework/lesson.py
@@ -0,0 +1,38 @@
+name = input()
+print("hello "+name)
+
+print(5+5)
+print(7+6)
+print(90+30)
+print(10+50)
+print(30+39)
+
+print(20-13)
+print(17-3)
+print(59-37)
+print(49-23)
+print(2-1)
+
+print(20*2)
+print(13*3)
+print(39*2)
+print(47*3)
+print(5*5)
+
+print(6/3)
+print(35/5)
+print(144/12)
+print(22/2)
+print(30/3)
+
+my_name = "saba"
+my_height = 173
+my_age = 15
+my_gender = "male"
+my_hobby = "basketball"
+
+print(my_name)
+print(my_height)
+print(my_age)
+print(my_gender)
+print(my_hobby)
