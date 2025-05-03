560aafa3a13832083bf07ebe2ea560d8c6cfed17
Saba-chancho <geosaba215@gmail.com>
2025-05-03 21:26:43 +0400

    commit

---
+++ day 04/classwork/lesson1.py
@@ -0,0 +1,4 @@
+#text info
+my_name = "saba"
+#number count 
+my_age = 15
--- day 04/homework/lesson.py
+++ day 04/homework/lesson.py
@@ -1,4 +1,4 @@
 my_name = "saba"
 my_surname = "chanchaleishvili" 
 my_age = 15
-print("hello, my name is " + my_name + " and my surname is " + my_surname + "," " I am " + str(my_age) + " year old" )
+print("Hello, my name is " + my_name + " and my surname is " + my_surname + "," " I am " + str(my_age) + " years old" )
