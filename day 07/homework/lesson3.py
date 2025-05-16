4dee9fa0966afd0f42c5a62f3022b649f9cdb7ba
Saba-chancho <geosaba215@gmail.com>
2025-05-16 15:05:11 +0400

    commit

---
+++ day 07/homework/lesson3.py
@@ -0,0 +1,5 @@
+x = int(input())
+if x%2 == 0 and x>10 or x == 7:
+    print("true")
+if x%2 != 0 and x>10 or x<10 :
+    print("false")
---
+++ day 07/homework/lesson4.py
@@ -0,0 +1,25 @@
+a = int(5)              
+b = int("10")          
+c = int(4.8)           
+
+print(a, b, c)          
+
+
+
+name = str("Nika")      
+age = str(15)          
+text = str(True)        
+
+print(name, age, text)  
+
+x = float(3)            
+y = float("7.5")        
+z = float(2.0 + 0.75)   
+
+print(x, y, z)         
+
+is_adult = bool(18 > 16)  
+is_empty = bool("")        
+has_money = bool(100)     
+
+print(is_adult, is_empty, has_money)  
