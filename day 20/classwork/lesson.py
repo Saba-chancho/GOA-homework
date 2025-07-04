6fdc0dbfa9ae52b5ca6f19093c0e728fe3653394
Saba-chancho <geosaba215@gmail.com>
2025-07-04 19:13:06 +0400

    commit

---
+++ day 20/classwork/lesson.py
@@ -0,0 +1,14 @@
+# ინდექსინგი გვაძლევს წვდომას კონკრეტულ ელემენტზე, მაგალითად: ასოზე სტრინგში ან ელემენტზე ლისტში
+listi = ["saba", "gio", "sandro", "irakli", "goga"]
+
+listi[0] = "jora"
+listi[1] = "qsenia"
+listi[2] = "valeri"
+listi[3] = "nikolozi"
+listi[4] = "gogita"
+print(listi[0])
+print(listi[1])
+print(listi[2])
+print(listi[3])
+print(listi[4])
+
