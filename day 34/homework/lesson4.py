1ee71c19179650e67050cb29038765b521392052
Saba-chancho <geosaba215@gmail.com>
2025-09-02 19:23:15 +0400

    commit

---
+++ day 34/homework/lesson4.py
@@ -0,0 +1,7 @@
+my_tuple = ("hello", "hello", "goa", "goa")
+
+hello = my_tuple.count("hello")
+goa = my_tuple.count("goa")
+
+print(f"hello გამოყენებულია {hello} ჯერ")
+print(f"goa გამოყენებულია {goa} ჯერ")
---
+++ day 36/classwork/lesson1.py
@@ -0,0 +1,9 @@
+about_me = {
+    "სახელი": "საბა",
+    "გვარი": "ჭანჭალეიშვილი",
+    "ასაკი": 15
+}
+print(about_me.keys())
+print(about_me.values())
+about_me["საყვარელი ფერი"] = "თეთრი"
+print(about_me)
